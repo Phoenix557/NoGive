64593600ea1d858f8aed92fdc94ecf0f408df310
Dylan <65882593+Phoenix557@users.noreply.github.com>
2022-07-09 17:31:24 -0400

    Add files via upload

---
+++ NoGive.cs
@@ -0,0 +1,19 @@
+﻿namespace Oxide.Plugins
+{
+    [Info("NoGive", "Phoenix", "1.0.0")]
+    [Description("Don't show give notices")]
+    public class NoGive : CovalencePlugin
+    {
+        #region OnServerMessage
+        object OnServerMessage(string message, string name)
+        {
+            if (message.Contains("gave") && name == "SERVER")
+            {
+                return true;
+            }
+            return null;
+        }
+        #endregion
+
+    }
+}
